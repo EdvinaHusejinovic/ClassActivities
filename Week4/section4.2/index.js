1213d8f6406405aa55b52318d9b57f6621ac6ea6
Edvina Husejinovic <edvinahusejinovic@gmail.com>
2023-12-02 13:11:12 -0600

    Concat and Join

---
+++ Week4/section4.2/index.js
@@ -0,0 +1,5 @@
+document.querySelector("#dog-picture").addEventListener("click", event => {
+  // this is not a one liner
+  console.log("Nav bar has been clicked");
+});
+// to make the index.js file work you need to add to html file <script src="index.js"></script>
