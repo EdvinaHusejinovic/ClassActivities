a05cef8946eadb6701107779f7832cd9e006c007
Edvina Husejinovic <edvinahusejinovic@gmail.com>
2023-12-02 13:06:52 -0600

    Event Listeners

--- Week4/section4.2/4.2.5-EventListeners.js
+++ Week4/section4.2/4.2.5-EventListeners.js
@@ -1,26 +1,40 @@
 // add an event listener to the dog picture that listens for a "click" and gives an alert
-const dogPicture = document.getElementById("dog-picture");
-
-dogPicture.addEventListener("click", () => {
-  alert("The dog picture was clicked!");
+document.querySelector("#dog-picture").addEventListener("click", event => {
+  console.log("Nav bar has been clicked");
 });
+
 // add an event listener to the list items in the class schedule that listens for a "mouseover" event and changes its background color
-const classScheduleListItems = document.querySelectorAll(
-  "#class-schedule-list li"
-);
-
-classScheduleListItems.forEach(listItem => {
-  listItem.addEventListener("mouseover", () => {
-    listItem.style.backgroundColor = "lightgray";
+const listItems = document.querySelectorAll("#class-schedule-list li");
+console.log(listItems);
+for (let item of listItems) {
+  item.addEventListener("mouseover", event => {
+    event.target.classList.toggle("bgred");
+    // event.target.innerText += "z";
   });
-
-  // Reset the background color on mouseout (optional)
-  listItem.addEventListener("mouseout", () => {
-    listItem.style.backgroundColor = "";
-  });
-});
+  item.addEventListener(
+    "mouseout",
+    event => (event.target.style.backgroundColor = "inherit")
+  );
+}
 
 // add an event listener to the whole page that listens for a "keydown" event and prints the key that was pressed
-document.addEventListener("keydown", event => {
-  console.log("Key pressed:", event.key);
+document.addEventListener("keyup", event => {
+  console.log(event.target);
+  switch (event.key) {
+    case "a":
+      console.log(`Move left`);
+      break;
+    case "d":
+      console.log(`Move right`);
+      break;
+  }
 });
+
+function clickHandler(event) {
+  console.log("clickHandler fired", event);
+}
+
+document.getElementById("navBar");
+    .addEventListener('click', (event) => {
+      window.location.href = "https: //www.google.com";
+    });
